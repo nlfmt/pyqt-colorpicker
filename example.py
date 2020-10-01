02166a7ceb6b40fc5a2c1dfe43fa2781e4e7fb3d
avazevfx <avazevfx@gmail.com>
2020-10-01 13:41:39 +0200

    Added example script

--- example.py
+++ example.py
@@ -3,10 +3,13 @@ from PyQt5.QtWidgets import QApplication
 
 app = QApplication([])
 
-my_color_picker = ColorPicker(lightTheme=False)
+my_color_picker = ColorPicker()
+my_color_picker_light = ColorPicker(lightTheme=True)
 
 old_color = (255,255,255)
 
 picked_color = my_color_picker.getColor(old_color)
+print(picked_color)
 
+picked_color = my_color_picker_light.getColor()
 print(picked_color)
